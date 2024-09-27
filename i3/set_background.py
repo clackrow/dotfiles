270e9d11155a381a963562c144f7d8c6e083e28f
Rodrigo <guigoazzolini@gmail.com>
2024-09-27 11:57:47 -0300

    yeh

--- i3/set_background.py
+++ i3/set_background.py
@@ -6,15 +6,15 @@ import time
 def main():
     os.system("notify-send 'bg changing routine is running'")
     while True:
+        print('Running change bg routine')
         hour = datetime.datetime.now().hour
-        if hour > 8 and hour < 12:
+        if hour < 8:
             os.system("feh --bg-scale ~/.config/i3/morning.png")
-        if hour > 12 and hour < 18:
+        if hour >= 8 and hour < 18:
             os.system("feh --bg-scale ~/.config/i3/day.png")
-        if hour > 18:
-            os.system("feh --bg-scale ~/.config/i3/night.png")
+        if hour >= 18:
+            os.system("feh --bg-scale ~/.config/i3/afternoon.png")
         time.sleep(300)
 
 
-if __name__ == "__main__":
-    main()
+main()
